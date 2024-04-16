04639a7ccc36b8a22c1dbcc635397d6f876e722c
jvanroy <jackstar9595@gmail.com>
2024-04-17 09:43:26 +1000

    reverted changes

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-cd /var/www/app
-sudo chmod g+s /var/www/app
-sudo chmod o-rwx /var/www/app
-npm install
+# Install node.js and PM2 globally
+sudo apt-get update
+sudo apt-get install nodejs-legacy -y
+sudo apt-get install npm  -y
+sudo npm install pm2 -g
