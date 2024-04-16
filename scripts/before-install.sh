2320e2e0167ca8722a2b634fbbedc1fc39997748
jvanroy <jackstar9595@gmail.com>
2024-04-17 07:51:59 +1000

    testing new alternative

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
-
-
-# Install EPEL repository (for CentOS, Amazon Linux)
-sudo yum install -y epel-release
-    
-# Install Node.js from EPEL repository
-sudo yum install -y nodejs npm
-
-
-# Install nodemon and ts-node globally
-sudo npm install -g nodemon ts-node
-sudo npm install
-
-# Check if nodemon and ts-node are installed successfully
-if ! command -v nodemon &> /dev/null || ! command -v ts-node &> /dev/null; then
-    echo "Error: nodemon or ts-node installation failed." >&2
-    exit 1
-fi
-
-echo "BeforeInstall script executed successfully."
+cd /var/www/app
+sudo chmod g+s /var/www/app
+sudo chmod o-rwx /var/www/app
+npm install
