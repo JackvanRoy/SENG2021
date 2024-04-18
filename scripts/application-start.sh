cfaaab740afb54b6eb00b341248daf413eac8f4f
jvanroy <jackstar9595@gmail.com>
2024-04-18 19:54:14 +1000

    modified script

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 cd /var/www/app
 
-pkill -f "nohup npm start"
+# Stop the currently running npm start process
+pkill -f "npm start"
+
+# Wait for the process to be killed
+sleep 1
 
 # Execute npm start in the background and detach it from the terminal
 nohup npm start > /dev/null 2>&1 &
